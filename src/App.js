349e741353f955135e5aefb541e581595b1d1056
Umar Farouq <umarfarouq447@gmail.com>
2024-11-18 16:53:33 +0100

    Adding navbar files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <NavBar />
         <h1>Hello welcome</h1>
         <p>Am coming</p>
       </BrowserRouter>
