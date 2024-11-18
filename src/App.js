a8fdb38feeee69d8d20df8994be5117c532fdaa1
Umar Farouq <umarfarouq447@gmail.com>
2024-11-18 14:30:06 +0100

    Initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <h1>Hello welcome</h1>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
