335f9ee4ee4a41c57bdca24206e187403e515bf0
Umar Farouq <umarfarouq447@gmail.com>
2024-11-20 13:56:12 +0100

    Portfolio done

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,6 +6,17 @@ const Portfolio = () => {
         <h2>Portfolio</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
       </div>
+      <div className="port">
+        <img src="/assets/images/mb1.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb2.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb3.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb4.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb5.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb6.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb7.png" alt="Portfolio Banners" />
+        <img src="/assets/images/mb8.png" alt="Portfolio Banners" />
+      </div>
+      <button className="work-btn">Learn More</button>
     </div>
   );
 };
